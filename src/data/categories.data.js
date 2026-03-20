167a46a0978718a662c75b0c6560e2250de944eb
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-03-19 20:40:52 -0500

    refactor(data): actualizar nombre de categoría en categoriesData
    Se modificó el archivo categoriesData.js para cambiar el nombre de una de las categorías. Cada objeto mantiene las propiedades id y name, pero se ajustó el valor de name para reflejar mejor la estructura actual.

--- src/data/categories.data.js
+++ src/data/categories.data.js
@@ -1,5 +1,5 @@
 const categoriesData = [
-    { id: 1, name: "Laptops" },
+    { id: 1, name: "Ordenadores" },
     { id: 2, name: "Periféricos" },
     { id: 3, name: "Componentes" },
     { id: 4, name: "Monitores" },
