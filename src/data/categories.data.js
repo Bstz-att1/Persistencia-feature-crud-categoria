602c0f557dce0c773e82116bbbb721b37476f642
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-03-19 19:51:32 -0500

    feat(data): agregar archivo categories.data.js con lista inicial de categorías

---
+++ src/data/categories.data.js
@@ -0,0 +1,9 @@
+const categoriesData = [
+    { id: 1, name: "Laptops" },
+    { id: 2, name: "Periféricos" },
+    { id: 3, name: "Componentes" },
+    { id: 4, name: "Monitores" },
+    { id: 5, name: "Accesorios" }
+];
+
+export default categoriesData;
